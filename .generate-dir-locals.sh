e18fbe36aeadea4f3f610a93f5db0fd08b78850d
Othmane AIT EL CADI <dartzon@gmail.com>
2018-07-29 12:29:00 +0100

    Remove cmake-ide references from .dir-locals.el

--- .generate-dir-locals.sh
+++ .generate-dir-locals.sh
@@ -9,18 +9,6 @@ _build_args=$5
 _gcc_version=$(gcc -dumpversion)
 
 _dir_locals="(( nil . (\n\t
-(cmake-ide-project-dir . \"$_project_path/\")\n\t
-(cmake-ide-build-dir . \"$_project_path/build/$_build_mode/$_build_arch/\")\n\t
-(cmake-ide-cmakelists-dir . \"$_project_path/\")\n\t
-(cmake-ide-compile-command . \"$_project_path/build.sh $_build_args\")\n\t
-(cmake-ide-flags-c . \"-I/usr/include\")\n\t
-(cmake-ide-flags-c++ . (\"-I/usr/include/c++/$_gcc_version\"\n\t\t
-                       \"-I/usr/include/c++/$_gcc_version/x86_64-pc-linux-gnu\"\n\t\t
-                       \"-I/usr/include/c++/$_gcc_version/backward\"\n\t\t
-                       \"-I/usr/lib/gcc/x86_64-pc-linux-gnu/$_gcc_version/include\"\n\t\t
-                       \"-I/usr/local/include\"\n\t\t
-                       \"-I/usr/lib/gcc/x86_64-pc-linux-gnu/$_gcc_version/include-fixed\"\n\t\t
-                       \"-I/usr/include\"))\n\t
 (flycheck-clang-tidy-build-path . \"$_project_path/build/$_build_mode/$_build_arch/\")\n\t
 (projectile-enable-caching . t)\n\t
 (projectile-project-name . \"$_project_name\")\n\t
